aba5f612c21f3463859ba35202d791e1cc4204e5
SameerAhmad-07 <sameerhallikeri4@gmail.com>
2025-10-06 14:53:02 +0530

    save the file

---
+++ temp.py
@@ -0,0 +1,9 @@
+temperature = float(input("Enter the temperature in °C: "))
+
+if temperature < 20:
+    condition = "Cold"
+elif 20 <= temperature <= 30:
+    condition = "Normal"
+else
+    condition = "Hot"
+print(f"The weather is {condition}.")
